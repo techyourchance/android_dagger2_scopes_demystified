bd56c8c356dc82e6739798df4875df580531dc0b
Vasiliy <techyourchance@gmail.com>
2016-10-22 11:38:45 +0300

    Initial AndroidStudio project + gitignore

---
+++ app/src/main/java/com/techyourchance/www/dagger2tutorial/MainActivity.java
@@ -0,0 +1,13 @@
+package com.techyourchance.www.dagger2tutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
